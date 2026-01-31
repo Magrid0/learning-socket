6b2bb08b990135a96c6e0ac72ff0fb1fdc2b0abb
Magrid0 <magrid0@proton.me>
2026-01-31 15:50:25 +0100

    -Make the client contact the server

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,20 @@
+import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         // IP and Port of the socket.
         String host = "127.0.0.1";
         int port = 5000;
 
-        Socket socket = new Socket(host, port); // Create the socket.
+        try {
+            Socket socket = new Socket(host, port); // Create the socket.
+            System.out.println("Connected to server.");
+
+        } catch (IOException e) {
+            System.err.println("Client error:");
+            e.printStackTrace();
+        }
     }
 }
+
--- src/Server.java
+++ src/Server.java
@@ -1,9 +1,31 @@
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Server {
-    public static void main(String [] args) {
+
+    public static void startServer(int port) { // Start the socket and display some verbose.
+        System.out.println("Starting server...");
+
+        try {
+            ServerSocket server = new ServerSocket(port);
+            System.out.println("Server started.");
+
+            Socket client = server.accept(); // Accept incoming connections.
+            System.out.println("Client connected.");
+
+        } catch (IOException e) {
+            System.err.println("Server error:");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
         // IP and Port of the socket.
         String host = "127.0.0.1";
-        int port = 5000;    
+        int port = 5000;
+
+        // Start server socket.
+        startServer(port);
     }
 }
+
