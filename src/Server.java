ccefffae16937789833b74dd4001be92becf2275
Magrid0 <magrid0@proton.me>
2026-01-31 15:33:58 +0100

    Split server and client in two different files

---
+++ src/Server.java
@@ -0,0 +1,9 @@
+import java.net.Socket;
+
+public class Server {
+    public static void main(String [] args) {
+        // IP and Port of the socket.
+        String host = "127.0.0.1";
+        int port = 5000;    
+    }
+}
